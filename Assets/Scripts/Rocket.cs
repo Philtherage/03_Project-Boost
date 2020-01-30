ad1c082b1c8a352dd5bc69bdcfc1666edca676ba
Philtherage <phillipwood61@gmail.com>
2020-01-29 22:10:14 -0500

    Section 3: 42. Basic Input Binding.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float thrusterForce = 1f;
+    [SerializeField] float rotateForce = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0f, thrusterForce, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(Vector3.forward, -rotateForce); 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward, rotateForce);
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 * Section 3: 40. The Origin Of Our World.
 
 * Section 3: 41. Placeholder Art From Primitives.
+
+* Section 3: 42. Basic Input Binding. (Challanged myself to get the ship moving and seems to work).
