4524543d19dc6162fe052900911badc2997b191e
Philtherage <phillipwood61@gmail.com>
2020-01-30 19:06:44 -0500

    Section 3: 43. Physics and Rigidbodies
    changed from velocity to relative force on thrust

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,10 +7,14 @@ public class Rocket : MonoBehaviour
     [SerializeField] float thrusterForce = 1f;
     [SerializeField] float rotateForce = 1f;
 
+
+
+    Rigidbody rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,16 +27,16 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0f, thrusterForce, 0f);
+            rigidBody.AddRelativeForce(Vector3.up * thrusterForce); 
         }
         if (Input.GetKey(KeyCode.A))
         {
             
-            transform.Rotate(Vector3.forward, -rotateForce); 
+            transform.Rotate(Vector3.forward, rotateForce); 
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward, rotateForce);
+            transform.Rotate(Vector3.forward, -rotateForce);
             
         }
     }
--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 * Section 3: 41. Placeholder Art From Primitives.
 
 * Section 3: 42. Basic Input Binding. (Challanged myself to get the ship moving and seems to work).
+
+* Section 3: 43. Physics and Rigidbodies. (Changed from velocity to relativeForce on thrust).
